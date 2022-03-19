7df796ed7e09b4a145f5108767c9885ca061b717
tambetejaswini <tambetejaswini400@gmail.com>
2022-03-19 11:10:56 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoList from "./ProjectComp/TodoList";
+import TodoApp from "./ProjectComp/TodoApp.css";
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList></TodoList>
+    </div>
+  );
+}
+export default App;
